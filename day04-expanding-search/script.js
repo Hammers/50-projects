eb5a8f1b7c2151f63cbf2e32d78a2e3fb3e0e1c4
Ryan Ayton <ryanianhamlet@gmail.com>
2021-01-06 14:11:42 +0000

    Added day 5 project and small tweak to day 4 project

--- day04-expanding-search/script.js
+++ day04-expanding-search/script.js
@@ -1,15 +1,9 @@
 const btn = document.getElementById('btn')
 const container = document.getElementById('container')
-
-let expanded = false;
+const input = document.getElementById('search')
 
 btn.addEventListener('click',() => {
-    if(!expanded){
-        expanded = true;
-        container.classList.add("active");
-    }
-    else {
-        expanded = false;
-        container.classList.remove("active");
-    }
+        container.classList.toggle("active");
+        input.focus()
+    
 })
