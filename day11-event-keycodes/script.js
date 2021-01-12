bbfca9600c35665e6f4cc6f51ea8637df81cfaf8
Ryan Ayton <ryanianhamlet@gmail.com>
2021-01-12 20:13:59 +0000

    Added day 11 project

---
+++ day11-event-keycodes/script.js
@@ -0,0 +1,14 @@
+const instructions = document.querySelector('#instructions');
+const key = document.querySelector('#key');
+const keyCode = document.querySelector('#keyCode');
+const code = document.querySelector('#code');
+
+window.addEventListener('keydown', event => {
+    instructions.classList.add('hidden');
+    key.parentElement.classList.remove('hidden');
+    key.innerText = event.key;
+    keyCode.parentElement.classList.remove('hidden');
+    keyCode.innerText = event.keyCode.toString();
+    code.parentElement.classList.remove('hidden');
+    code.innerText = event.code;
+})
